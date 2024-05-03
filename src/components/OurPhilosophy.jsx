0013a6347d090b7f93fbc0eaca82c8aabe639004
sergej_99 <sergejmalesevic8@gmail.com>
2024-05-03 17:30:51 +0200

    AboutUs_second

--- src/components/OurPhilosophy.jsx
+++ src/components/OurPhilosophy.jsx
@@ -11,22 +11,22 @@ const OurPhilosophy = () => {
 
       <div className='textPart'>
         <h1>Our philosophy:</h1>
-        <p className='paraf'>
+        <p className='text_Paraf'>
           <span className='footerText'>Passion</span> is the driving force behind our work. 
           This enthusiasm fuels our commitment to delivering exceptional results for our clients.
           Whether it's crafting compelling content, devising innovative marketing strategies, or following the latest industry trends, our passion shines through in every project we undertake.
         </p>
-        <p className='paraf'>
+        <p className='text_Paraf'>
           <span className='footerText'>Teamwork</span> is not just a buzzword for us it is the foundation of everything we do.
           Our team is the heart of our agency a diverse and dynamic group of individuals who bring a wealth of expertise to the table.
           By fostering a positive environment for collaboration and communication, we ensure that every project benefits from the power of our team's collective intelligence and creativity.
         </p>
-        <p className='paraf'>
+        <p className='text_Paraf'>
           <span className='footerText'>Partnership</span> We pride ourselves on building strong, collaborative partnerships with our clients.
           We strongly believe in partnerships where your success is not just a goal but a shared journey.
           Thank you for considering our digital marketing agency for your marketing needs.
         </p>
-        <p className='paraf pbold'>
+        <p className='text_Paraf'>
           We are excited to combine our passion, love, skills, and knowledge to help you succeed.
         </p>
       </div>
