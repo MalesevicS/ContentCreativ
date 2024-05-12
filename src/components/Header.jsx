a93de73a243c93e671cbbaba4acb8f0283c09ffa
sergej_99 <sergejmalesevic8@gmail.com>
2024-05-12 19:40:01 +0200

    header_change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import "../styles/Header.css"
-import logo from "../images/logo.png"
+import "../styles/Header.css";
+import logo from "../images/logo.png";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [isExpanded, setIsExpanded] = useState(false); // Dodajemo stanje za proširenje navbar-a
 
   const toggleNavbar = () => {
     setIsOpen(!isOpen);
-    setIsExpanded(!isExpanded); // Invertujemo stanje proširenja navbar-a
   };
 
   return (
-    <nav className={`navbar ${isExpanded ? 'expanded' : ''}`}> {/* Dodajemo klasu expanded kada je navbar proširen */}
+    <nav className={`navbar ${isOpen ? 'expanded' : ''}`}>
       <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          <img className='logoPic' src={logo} alt="#" />
-        </Link>
+        <div className={`logo ${isOpen ? 'hidden' : ''}`}>
+          <Link to="/" className="navbar-logo">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </div>
         <div className={`navbar-links ${isOpen ? 'active' : ''}`}>
           <Link to="/" onClick={toggleNavbar}>Home</Link>
           <Link to="/about" onClick={toggleNavbar}>About</Link>
-          <Link to="/reference" onClick={toggleNavbar}>Services</Link>
+          <Link to="/services" onClick={toggleNavbar}>Services</Link>
         </div>
         <div className="navbar-toggle" onClick={toggleNavbar}>
           <span></span>
